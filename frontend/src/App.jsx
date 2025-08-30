9279413950869516a81641de8d46ea4546eda743
Sam Stradling <sds27@st-andrews.ac.uk>
2025-08-31 00:21:03 +0100

    Added dropdown component from react bootstrap. Made all necessary accommodating changes for using bootstrap, including slightly changing the header css file. A plain drop down box is now on the page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles/App.css";
 import Header from "./components/Header"
+import DropDownButton from "./components/DropDownButton";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <p>writing<br/></p>
         <p>writing<br/></p>
         <p>writing<br/></p>
+        <DropDownButton />
         <p>writing<br/></p>
         <p>writing<br/></p>
         <p>writing<br/></p>
